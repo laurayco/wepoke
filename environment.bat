4281553541249fb7ed2f866b6875c58d53fd3bd4
Full Metal <tyler9725@gmail.com>
2013-08-23 19:55:49 -0500

    Setting up for scripting.

--- environment.bat
+++ environment.bat
@@ -1,4 +1,4 @@
-@start coffee -wc js/main.coffee js/game.coffee js/input.game.coffee js/utilities.game.coffee
+@start coffee -wc js/main.coffee js/game.coffee js/input.game.coffee js/utilities.game.coffee js/script.game.coffee
 @start serve -p 8080
 @ping 127.0.0.1
 @start http://127.0.0.1:8080
